c25a9748e99d0945255158d4fa4f6390604c297d
LouConreux <louis.conreux1@gmail.com>
2025-03-04 14:26:14 -0800

    [edit] Change get_colored_objects logic based on mask of HSV and mapping to dominant color

--- ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/locobot_autonomy/vision_detection.py
+++ ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/locobot_autonomy/vision_detection.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 from PIL import Image as ImageDraw
 from google.cloud import vision
-import colorsys
+import cv2
 
 JSON_KEY_PATH = "/home/ubuntu/Desktop/collaborative/keys/tomtom_key.json"
 
@@ -84,6 +84,18 @@ class VisionObjectDetector:
         """
         colored_objects = []
 
+        color_categories = ["red", "orange", "yellow", "green", "light blue", "dark blue", "purple"]
+    
+        color_ranges = {
+            "red": [([0, 100, 100], [10, 255, 255]), ([160, 100, 100], [180, 255, 255])],  # Red wraps around hue space
+            "orange": [([11, 100, 100], [25, 255, 255])],
+            "yellow": [([26, 100, 100], [35, 255, 255])],
+            "green": [([36, 100, 100], [85, 255, 255])],
+            "light blue": [([86, 100, 100], [110, 255, 255])],
+            "dark blue": [([111, 100, 100], [130, 255, 255])],
+            "purple": [([131, 100, 100], [159, 255, 255])]
+        }
+
         image = vision.Image(content=image_bytes)
 
         response = self.client.object_localization(image=image)
@@ -93,31 +105,58 @@ class VisionObjectDetector:
         pil_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
         width, height = pil_image.size
         image_np = np.array(pil_image)
+        hsv_image = cv2.cvtColor(image_np, cv2.COLOR_RGB2HSV)
+
 
         for obj in objects:
             vertices = obj.bounding_poly.normalized_vertices
 
-            x_min = vertices[0].x * width
-            y_min = vertices[0].y * height
-            x_max = vertices[2].x * width
-            y_max = vertices[2].y * height
-
-            center_x = (x_min + x_max) / 2
-            center_y = (y_min + y_max) / 2
+            x_coords = [int(vertex.x * width) for vertex in vertices]
+            y_coords = [int(vertex.y * height) for vertex in vertices]
             
-            cropped_region = image_np[y_min:y_max, x_min:x_max]
-
-            avg_color = np.mean(cropped_region, axis=(0, 1)).astype(int)  # (R, G, B)
-            avg_color_tuple = tuple(avg_color)
-
-            avg_color_norm = tuple(c / 255.0 for c in avg_color)
-            hue, sat, val = colorsys.rgb_to_hsv(*avg_color_norm)
-            hue_deg = hue * 360 
-
+            min_x, max_x = max(0, min(x_coords)), min(width, max(x_coords))
+            min_y, max_y = max(0, min(y_coords)), min(height, max(y_coords))
+            
+            obj_region = hsv_image[min_y:max_y, min_x:max_x]
+            
+            if obj_region.size == 0:
+                continue
+            
+            avg_hsv = np.mean(obj_region, axis=(0, 1))
+            
+            # Determine the dominant color
+            dominant_color = None
+            for color, ranges in color_ranges.items():
+                for (lower, upper) in ranges:
+                    lower = np.array(lower)
+                    upper = np.array(upper)
+                    
+                    if all(lower <= avg_hsv) and all(avg_hsv <= upper):
+                        dominant_color = color
+                        break
+                
+                if dominant_color:
+                    break
+            
+            # If no dominant color is found, choose the closest one
+            if not dominant_color:
+                hue = avg_hsv[0]
+                min_distance = 180  # Maximum possible hue distance
+                
+                for color in color_categories:
+                    for (lower, upper) in color_ranges[color]:
+                        middle_hue = (lower[0] + upper[0]) / 2
+                        distance = min(abs(hue - middle_hue), 180 - abs(hue - middle_hue))
+                        
+                        if distance < min_distance:
+                            min_distance = distance
+                            dominant_color = color
+            
             colored_objects.append({
                 "name": obj.name,
-                "bounding_box": (x_min, y_min, x_max, y_max),
-                "color": avg_color_tuple,
-                "hue": hue_deg
+                "color": dominant_color,
+                "bounding_box": {
+                    "vertices": [(vertex.x, vertex.y) for vertex in vertices]
+                }
             })
         return colored_objects
